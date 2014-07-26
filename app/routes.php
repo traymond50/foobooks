6afd70de72603b7ac35f5867b5370eef8a80264c
Tim Raymond <traymond50@gmail.com>
2014-07-25 21:07:20 -0400

    Lecture 7 update

--- app/routes.php
+++ app/routes.php
@@ -9,18 +9,26 @@ Route::get('/', function(){
 //List books / search results of books
 Route::get('/list/{format?}', function($format = 'html'){
 
-	// Set the path
-	$path = app_path().'/database/books.json';
+	#Instantiating an object of the Library class
+	$library = new Library(app_path().'/database/books.json');
 
-	// Load the json file
-	$books = File::get($path);
- 
-	// Convert the string of JSON into object
-	$books = json_decode($books,true);
+	$query = Input::get('query');
 
-	//Default - HTML
+	#If there is a query, search the library with that query
+	if($query) {
+		$books = $library->search($query);
+	}
+	#Otherwise, just fetch all books
+	else {
+		$books = $library->get_books();
+	}
+
+	#Decide on output method...
+	#Default - HTML
 	if($format == 'html'){
-		return View::make('list')->with('books',$books);
+		return View::make('list')
+				->with('books', $books)
+				->with('query', $query);
 	}
 
 	//JSON
@@ -75,4 +83,13 @@ Route::get('/data', function(){
 
 	//Output so we can check it out
 	return Paste\Pre::render($books,'Books');
+});
+
+//check to make sure that database connection is working
+Route::get('mysql-test', function(){
+	#Use the DB component to select all the databases
+	$results = DB::select('SHOW DATABASES;');
+
+	#If the "Pre" package is not installed, you should output using print_r instead
+	return Paste\Pre::render($results);
 });
